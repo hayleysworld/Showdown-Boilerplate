71cc97334d80aca5e1d193cab2e139060ef4faef
MeliodasBH <spunik911@gmail.com>
2016-03-05 21:10:10 -0500

    Update dnsbl.js

--- dnsbl.js
+++ dnsbl.js
@@ -14,7 +14,7 @@
 
 'use strict';
 
-const BLOCKLISTS = ['sbl.spamhaus.org', 'rbl.efnetrbl.org', 'zenmate.proxy-nohos', 'secured-by.zenmate.com'];
+const BLOCKLISTS = ['sbl.spamhaus.org', 'rbl.efnetrbl.org', 'zenmate.proxy-nohos', 'secured-by.zenmate.com', 'dynamic.voo.be'];
 
 let dns = require('dns');
 
