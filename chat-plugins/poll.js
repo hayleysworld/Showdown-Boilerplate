89248b277771eee7fdd08705b388c35fae239ca8
MeliodasBH <spunik911@gmail.com>
2016-03-08 15:34:15 -0500

    Update poll.js

--- chat-plugins/poll.js
+++ chat-plugins/poll.js
@@ -301,7 +301,7 @@ exports.commands = {
 
 	},
 	tourpoll: function(target, room, user) {
-		var tiers = ['Battle Factory', 'Metagamiate', 'CC 1V1', 'Hackmons', 'Unreleased OU', 'Monotype', 'OU', 'UU', 'Random Battle'];
+		var tiers = ['Battle Factory', 'Metagamiate', 'CC 1V1', 'Hackmons', 'Monotype', 'OU', 'UU', 'Random Battle'];
 		this.parse('/poll new Next Tournament?, ' + tiers);
      	},
        leaguepoll: function(target, room, user) {
